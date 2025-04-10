0751c656cc15ecb43f2e6f67d428601392b920a2
Ruben Croall <122880204+Rubbaducky95@users.noreply.github.com>
2025-04-10 18:51:41 +0200

    Update SD_card.cpp
    Save lastLogNumber internally on the flash drive to fetch on startup creating new log files without iterating through al of them

--- LoRa_CAN_read_and_send/SD_card.cpp
+++ LoRa_CAN_read_and_send/SD_card.cpp
@@ -7,6 +7,7 @@
 #include "SD_card.h"
 #include <string.h>
 #include <time.h>
+#include <nvs_flash.h>
 
 const char *filePath;
 
@@ -76,6 +77,7 @@ void readFile(fs::FS &fs, const char * path){
 }
 
 void writeFile(fs::FS &fs, const char * path, const char * message){
+
   Serial.printf("Writing file: %s\n", path);
 
   File file = fs.open(path, FILE_WRITE);
@@ -92,7 +94,8 @@ void writeFile(fs::FS &fs, const char * path, const char * message){
 }
 
 void appendFile(fs::FS &fs, const char * path, const char * message){
-  //Serial.printf("Appending to file: %s\n", path);
+
+  Serial.printf("Appending to file: %s\n", path);
 
   File file = fs.open(path, FILE_APPEND);
   if(!file){
@@ -100,11 +103,67 @@ void appendFile(fs::FS &fs, const char * path, const char * message){
     return;
   }
   if(file.print(message)){
-      //Serial.println("Message appended");
+    Serial.println("Message appended");
   } else {
     Serial.println("Append failed");
   }
   file.close();
+}
+
+void write2SDcard(const char* msg, int flag) {
+
+  int8_t cardType = SD.cardType();
+  if(cardType == CARD_NONE){
+    Serial.println("No SD card attached");
+    return;
+  }
+
+  //uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  //Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+  static char lastFilePath[30] = "/log_1.txt";  // Store last used file path
+  char newFilePath[30];
+
+  // Using flash memory to fetch last used lognumber
+  
+  if (flag) {
+    // Retrieve the last log number from NVS
+    int lastLogNumber = getLastLogNumber();
+    int fileNumber = lastLogNumber + 1;
+
+    // Generate a unique filename
+    snprintf(newFilePath, sizeof(newFilePath), "/log_%d.txt", fileNumber);
+    strcpy(lastFilePath, newFilePath); // Store last created file path
+
+    // Update the last log number in NVS
+    setLastLogNumber(fileNumber);
+
+    writeFile(SD, newFilePath, msg);
+  } else {
+    // Append to last used file
+    appendFile(SD, lastFilePath, msg);
+  }
+
+  // Iterating through files until one is found that has not been used
+  /* if (flag) { 
+    // Generate a unique filename
+    int fileNumber = 1;
+    do {
+      snprintf(newFilePath, sizeof(newFilePath), "/log_%d.txt", fileNumber);
+      fileNumber++;
+      //Serial.printf("Checking file: %s\n", newFilePath);
+    } while (SD.exists(newFilePath)); // Keep incrementing until we find a non-existing file
+    
+    strcpy(lastFilePath, newFilePath); // Store last created file path
+    writeFile(SD, newFilePath, msg);
+  } else {
+    // Append to last used file
+    appendFile(SD, lastFilePath, msg);
+  } */
+
+  //Serial.printf("Using file: %s\n", lastFilePath);
+  //Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+  //Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
 }
 
 void renameFile(fs::FS &fs, const char * path1, const char * path2){
@@ -167,41 +226,53 @@ void testFileIO(fs::FS &fs, const char * path){
   file.close();
 }
 
-void write2SDcard(const char* msg, int flag) {
+bool initNVS() {
+  // Initialize NVS
+  esp_err_t err = nvs_flash_init();
+  if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    // NVS partition was truncated and needs to be erased
+    const esp_partition_t* nvs_partition = esp_partition_find_first(
+      ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_NVS, NULL);
+    if (nvs_partition) {
+      err = esp_partition_erase_range(nvs_partition, 0, nvs_partition->size);
+      if (err != ESP_OK) {
+        Serial.println("Failed to erase NVS partition");
+        return false;
+      }
+    }
+    // Retry nvs_flash_init
+    err = nvs_flash_init();
+  }
+  return err == ESP_OK;
+}
 
-  if(!SD.begin(CS)){ //If no connection on pin 14
-    Serial.println("Card Mount Failed");
+int getLastLogNumber() {
+  int32_t lastLogNumber = 0;
+  nvs_handle_t my_handle;
+  esp_err_t err = nvs_open(NVS_NAMESPACE, NVS_READWRITE, &my_handle);
+  if (err != ESP_OK) {
+    Serial.println("Error opening NVS");
+    return 0;
+  }
+  err = nvs_get_i32(my_handle, NVS_KEY, &lastLogNumber);
+  if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) {
+    Serial.println("Error reading last log number");
+  }
+  nvs_close(my_handle);
+  return lastLogNumber;
+}
+
+void setLastLogNumber(int logNumber) {
+  nvs_handle_t my_handle;
+  esp_err_t err = nvs_open(NVS_NAMESPACE, NVS_READWRITE, &my_handle);
+  if (err != ESP_OK) {
+    Serial.println("Error opening NVS");
     return;
   }
-
-  int8_t cardType = SD.cardType();
-  if(cardType == CARD_NONE){
-    Serial.println("No SD card attached");
-    return;
+  err = nvs_set_i32(my_handle, NVS_KEY, logNumber);
+  if (err != ESP_OK) {
+    Serial.println("Error writing last log number");
   }
-
-  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-  //Serial.printf("SD Card Size: %lluMB\n", cardSize);
-
-  static char lastFilePath[30] = "/log_1.txt";  // Store last used file path
-  char newFilePath[30];
-
-  if (flag) { 
-    // Generate a unique filename
-    int fileNumber = 1;
-    do {
-      snprintf(newFilePath, sizeof(newFilePath), "/log_%d.txt", fileNumber);
-      fileNumber++;
-    } while (SD.exists(newFilePath)); // Keep incrementing until we find a non-existing file
-    
-    strcpy(lastFilePath, newFilePath); // Store last created file path
-    writeFile(SD, newFilePath, msg);
-  } else {
-    // Append to last used file
-    appendFile(SD, lastFilePath, msg);
-  }
-
-  //Serial.printf("Using file: %s\n", lastFilePath);
-  //Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
-  //Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+  nvs_commit(my_handle);
+  nvs_close(my_handle);
 }
